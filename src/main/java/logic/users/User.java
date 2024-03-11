8e7231fbb460488af18ff17fda3d8bf80dccde6a
JRB958 <141108231+JRB958@users.noreply.github.com>
2024-03-11 16:28:56 -0400

    update the user class with complete entering-leaving rooms fuctionality

--- src/main/java/logic/users/User.java
+++ src/main/java/logic/users/User.java
@@ -13,39 +13,77 @@ public abstract class User {
     List<Permissions> permissions;
     String name;
     Room room = null;
+
+    //non-default constructor
     public User(String name){
         permissions = new ArrayList<>();
         this.name = name;
     }
 
+    // PERMISSION MANAGEMENT FUNCTIONS
+    // check the user has a specific permission, function to be used in SHC
     public Boolean hasPermission(Permissions permission){
         return permissions.contains(permission);
     }
-
+    // get user's permissions
     public List<Permissions> getPermissions() {
         return permissions;
     }
-
+    // assign user permissions
     public void setPermissions(List<Permissions> permissions) {
         this.permissions = permissions;
     }
+
+
+    //TODO check with Wade if it's cool!
+    /**  when user changes rooms:
+    * - if he's in a room he leaves it and enters the new room and update console
+    * - if he's outside he enters the room and update console.
+    */ 
+    public void moveToRoom(Room toRoom){
+        if(this.room != null){
+            exitRoom();
+            enterRoom(toRoom);       
+            System.out.println(name + " is in this room now: " + this.room.getName());
+        }
+        else{
+            enterRoom(toRoom);
+            System.out.println(name + " is in this room now: " + this.room.getName());       
+        }
+    }
+
+    /**  when user enters the room:
+    * - the room is assigned as his room
+    * - update the console
+    * - update the room with the new user inside it
+    * - turn the light on in the room only if it was off before
+    */ 
     public void enterRoom(Room room){
         this.room = room;
+        System.out.println( "entering " + this.room.getName());
         room.addUserToRoom(this);
         room.turnLightOn();
     }
-    public void exitRoom(Room room){
-        room.removeUserFromRoom(this);
-        room.turnLightOff();
+    
+    /**  when user leaves the room he's already in:
+    * - he's removed from the user list in the room
+    * - try to turn light off in the room is user was last to leave
+    * - set the user's current room to null (incase they are away from the house)
+    */ 
+    public void exitRoom(){
+        System.out.println( "exiting " + this.room.getName());
+        this.room.removeUserFromRoom(this);
+        this.room.turnLightOff();
+        this.room = null; //FIXME needed if the user leaves the HOUSE 
     }
+
+    // getters and setters
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public Room getRoom() {
         return room;
     }

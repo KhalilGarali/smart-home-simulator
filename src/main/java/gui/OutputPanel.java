98e29ddd650e4c883b44764802b2dff7b18ff77b
MinhDuc1711 <99374119+MinhDuc1711@users.noreply.github.com>
2024-03-26 14:10:53 -0400

    Added output console ouputs to log file, updated git ignore

--- src/main/java/gui/OutputPanel.java
+++ src/main/java/gui/OutputPanel.java
@@ -1,6 +1,9 @@
 package main.java.gui;
 
 import java.awt.Dimension;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
@@ -17,7 +20,20 @@ public class OutputPanel extends JPanel {
     private JTextArea outputConsole;
     private static OutputPanel op;
     private SHS shs = SHS.getInstance();
+    private File file;
+    private FileWriter writer;
     public OutputPanel() {
+        file = new File("consoleLog.txt");
+        if (file.exists()) {
+            file.delete();
+        }
+        try {
+            boolean isCreated = file.createNewFile();
+            writer = new FileWriter(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         setBorder(BorderFactory.createTitledBorder("Console Output"));
         outputConsole = new JTextArea();
@@ -28,25 +44,29 @@ public class OutputPanel extends JPanel {
         add(scrollPane);
     }
 
-    // Provide a method to append text to the output console
-    public void appendText(String text) {
-        // String date = DateTime.getDate().format(DateTimeFormatter.ofPattern("E MMM dd yyyy"));
-        // String time = DateTime.getTime().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
-        // outputConsole.append(date + " " + time + "\n");
+    // Append text to the output console and file
+    public void writeText(String text) {
+        try {
+            writer.write(text + "\n");
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         outputConsole.append(text + "\n");
     }
 
     public void appendText(ArrayList<String> text){
         String date = DateTime.getDate().format(DateTimeFormatter.ofPattern("E MMM dd yyyy"));
         String time = DateTime.getTime().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
-        outputConsole.append(date + " " + time + "\n");
+        writeText(date + " " + time);
         for (String txt: text){
-            outputConsole.append(txt + "\n");
+            writeText(txt);
         }
         // This can be subject to change if event is triggered not through GUI
-        outputConsole.append("Event triggered by user " + shs.activeUser.getName() + "\n");
-        outputConsole.append("**************************************************************\n");
+        writeText("Event triggered by user " + shs.activeUser.getName());
+        writeText("**************************************************************");
     }
+
     public static OutputPanel getInstance(){
         if (op == null){
             op = new OutputPanel();
